3bd052cf12d6a398aee8c81133a55e0b985b46ba
AlecVosika <alecvosika@comcast.net>
2020-02-04 18:01:32 -0600

    Added version2 of website

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,68 @@
-$('.x').on('click', function() {
-  $(this).siblings().removeClass('active');
-  $(this).addClass('active');
-});
+function showObjective() {
+    var e = document.getElementById("education");
+    e.style.display = "none";
+//    var c = document.getElementById("coursework");
+//    c.style.display = "none";
+    var s = document.getElementById("skills");
+    s.style.display = "none";
+//    var p = document.getElementById("projects");
+//    p.style.display = "none";
+    var o = document.getElementById("objective");
+    o.style.display = "";
+}
+
+
+function showEducation() {
+    var o = document.getElementById("objective");
+    o.style.display = "none";
+    var s = document.getElementById("skills");
+    s.style.display = "none";
+//    var c = document.getElementById("coursework");
+//    c.style.display = "none";
+//    var p = document.getElementById("projects");
+//    p.style.display = "none";
+    var e = document.getElementById("education");
+    e.style.display = "";
+}
+
+
+function showCoursework() {
+    var o = document.getElementById("objective");
+    o.style.display = "none";
+    var e = document.getElementById("education");
+    e.style.display = "";
+    var s = document.getElementById("skills");
+    s.style.display = "none";
+//    var p = document.getElementById("projects");
+//    p.style.display = "none";
+//    var c = document.getElementById("coursework");
+//    c.style.display = "";
+}
+
+
+function showSkills() {
+    var o = document.getElementById("objective");
+    o.style.display = "none";
+    var e = document.getElementById("education");
+    e.style.display = "none";
+//    var c = document.getElementById("coursework");
+//    c.style.display = "";
+//    var p = document.getElementById("projects");
+//    p.style.display = "none";
+    var s = document.getElementById("skills");
+    s.style.display = "";
+}
+
+
+function showProjects() {
+    var o = document.getElementById("objective");
+    o.style.display = "none";
+    var e = document.getElementById("education");
+    e.style.display = "none";
+    var c = document.getElementById("coursework");
+    c.style.display = "none";
+    var s = document.getElementById("skills");
+    s.style.display = "none";
+    var p = document.getElementById("projects");
+    p.style.display = "";
+}
---
+++ scripts/selected.js
@@ -0,0 +1,13 @@
+document.addEventListener('DOMContentLoaded', function () {
+    // do stuff here
+    // Add active class to the current button (highlight it)
+    var header = document.getElementById("grid-container");
+    var btns = header.getElementsByClassName("btn");
+    for (var i = 0; i < btns.length; i++) {
+        btns[i].addEventListener("click", function () {
+            var current = document.getElementsByClassName("active");
+            current[0].className = current[0].className.replace(" active", "");
+            this.className += " active";
+        });
+    }
+}, false);
